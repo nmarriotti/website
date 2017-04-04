b27f15eed37d548b84f7cadb8e0fd3300d428964
Nick Marriotti <nmarriotti@gmail.com>
2017-04-03 17:57:44 -0700

    Updated

--- scripts/content.py
+++ scripts/content.py
@@ -14,7 +14,7 @@ def Content():
     "C++":[["Installing the C++ compiler and Code:Blocks IDE","installing-compiler-and-codeblocks-ide"],
     ["Creating a Hello World program in C++!","hello-world"],
     ["Getting user input and variables","user-input-and-variables"],
-    ["Data Types","data_types"]],
+    ["Data Types","data-types"]],
     "Microsoft Windows":[["How to use Linux terminal commands in Windows","how-to-use-linux-terminal-commands-in-windows"]]}
 
     return TOPIC_DICT
--- scripts/init_code_creator.py
+++ scripts/init_code_creator.py
@@ -6,7 +6,15 @@ FUNC_TEMPLATE = '''
 
 @app.route("/tutorials/CURRENTTOPICLOW/"+TOPIC_DICT["CURRENTTOPIC"][CURRENTINDEX][1], methods=['GET', 'POST'])
 def CURRENTTITLE():
-    return render_template("tutorials/CURRENTTOPICLOW/CURRENTHTML", curLink = TOPIC_DICT["CURRENTTOPIC"][CURRENTINDEX][1], curTitle=TOPIC_DICT["CURRENTTOPIC"][CURRENTINDEX][0], title=TOPIC_DICT["CURRENTTOPIC"][CURRENTINDEX][0], curTopic="CURRENTTOPIC",  nextLink = TOPIC_DICT["CURRENTTOPIC"][NEXTINDEX][1], nextTitle = TOPIC_DICT["CURRENTTOPIC"][NEXTINDEX][0], TOPIC_DICT = TOPIC_DICT)
+    return render_template("CURRENTTOPIC/CURRENTHTML", curLink = TOPIC_DICT["CURRENTTOPIC"][CURRENTINDEX][1], curTitle=TOPIC_DICT["CURRENTTOPIC"][CURRENTINDEX][0],  nextLink = TOPIC_DICT["CURRENTTOPIC"][NEXTINDEX][1], nextTitle = TOPIC_DICT["CURRENTTOPIC"][NEXTINDEX][0], curTopic = "CURRENTTOPIC", title = TOPIC_DICT["CURRENTTOPIC"][CURRENTINDEX][0], TOPIC_DICT = TOPIC_DICT, TUTORIAL_CAT = TUTORIAL_CAT)
+
+'''
+
+FUNC_TEMPLATE_LAST = '''
+
+@app.route("/tutorials/CURRENTTOPICLOW/"+TOPIC_DICT["CURRENTTOPIC"][CURRENTINDEX][1], methods=['GET', 'POST'])
+def CURRENTTITLE():
+    return render_template("CURRENTTOPIC/CURRENTHTML", curLink = TOPIC_DICT["CURRENTTOPIC"][CURRENTINDEX][1], curTitle=TOPIC_DICT["CURRENTTOPIC"][CURRENTINDEX][0], curTopic = "CURRENTTOPIC", title = TOPIC_DICT["CURRENTTOPIC"][CURRENTINDEX][0], TOPIC_DICT = TOPIC_DICT, TUTORIAL_CAT = TUTORIAL_CAT, nextTitle= "None")
 
 '''
 
@@ -20,10 +28,13 @@ for each_topic in TOPIC_DICT:
             CURRENTTOPIC = each_topic
             CURRENTTITLE = eachele[0].replace("-","_").replace(" ","_").replace(",","").replace("/","").replace(")","").replace("(","").replace(".","").replace("!","").replace(":","-").replace("'","")
             CURRENTINDEX = str(index_counter)
-            NEXTINDEX = str(index_counter + 1)
             index_counter += 1
-
-            print( FUNC_TEMPLATE.replace("CURRENTTOPICLOW",CURRENTTOPIC.lower()).replace("CURRENTTOPIC",CURRENTTOPIC).replace("CURRENTINDEX",CURRENTINDEX).replace("CURRENTTITLE",CURRENTTITLE).replace("CURRENTHTML",CURRENTHTML).replace("NEXTINDEX",NEXTINDEX) )
+            NEXTINDEX = str(index_counter)
+            try:
+                if eachele[index_counter]:
+                    print( FUNC_TEMPLATE.replace("CURRENTTOPICLOW",CURRENTTOPIC.lower()).replace("CURRENTTOPIC",CURRENTTOPIC).replace("CURRENTINDEX",CURRENTINDEX).replace("CURRENTTITLE",CURRENTTITLE).replace("CURRENTHTML",CURRENTHTML).replace("NEXTINDEX",NEXTINDEX) )
+            except:
+                print( FUNC_TEMPLATE_LAST.replace("CURRENTTOPICLOW",CURRENTTOPIC.lower()).replace("CURRENTTOPIC",CURRENTTOPIC).replace("CURRENTINDEX",CURRENTINDEX).replace("CURRENTTITLE",CURRENTTITLE).replace("CURRENTHTML",CURRENTHTML) )
 
         except Exception as e:
             print(str(e))
