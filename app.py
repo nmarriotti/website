04e2b99db2f7f418302c8f84320d08d4332e4e2d
Nick Marriotti <nmarriotti@gmail.com>
2017-04-16 20:35:30 -0700

    Added compression

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, flash, render_template, request, redirect, url_for, g, 
 from functools import wraps
 from models.models import *
 from scripts.content import *
+from whitenoise import WhiteNoise
 from werkzeug.utils import secure_filename
 import os
 
@@ -14,59 +15,61 @@ TUTORIAL_CAT = TutorialCategories()
 TOPIC_DICT = Content()
 BLOG_TOPIC_DICT = BlogContent()
 
-app = Flask(__name__, static_url_path='/static')
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = 'some_secret'
+flap = Flask(__name__)
+app = WhiteNoise(flap, root='/static')
+#app = Flask(__name__, static_url_path='/static')
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#app.secret_key = 'some_secret'
 
 
-@app.before_request
+@flap.before_request
 def before_request():
 	# create db if needed and connect
 	initialize_db()
 
-@app.teardown_request
+@flap.teardown_request
 def teardown_request(exception):
 	# close the db connection
 	db.close()
 
-@app.errorhandler(404)
+@flap.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
 
-@app.route('/')
+@flap.route('/')
 def home():
 	# render the home page
 	return render_template('index.html', title='Home')
 
-@app.route('/contact')
+@flap.route('/contact')
 def contact():
 	# render the contact page
 	return render_template('contact.html', title="Contact")
 
-@app.route('/resume')
+@flap.route('/resume')
 def resume():
 	# render the resume page
 	return render_template('resume.html', title='Resume')
 
-@app.route('/tutorials/<title>/')
-@app.route('/tutorials/', defaults= {'title':1})
+@flap.route('/tutorials/<title>/')
+@flap.route('/tutorials/', defaults= {'title':1})
 def tutorials(title):
     if title == 1:
         return render_template("portfolio.html", title='Tutorials', TUTORIAL_CAT = TUTORIAL_CAT, count=0, TOPIC_DICT = TOPIC_DICT)
     else:
         return render_template(title+".html", title=str.title(title), TOPIC_DICT = TOPIC_DICT)
 
-@app.route('/profile')
+@flap.route('/profile')
 def profile():
 	# render the profile page
 	return render_template('profile.html', title='Profile')
 
-@app.route('/blog')
+@flap.route('/blog')
 def blog():
 	# render the blog page
 	return render_template('blog.html', posts=Post.select().order_by(Post.date.desc()), categories=Post.select(Post.category).distinct().order_by(Post.category.asc()), title = 'Blog', BLOG_TOPIC_DICT=BLOG_TOPIC_DICT, TUTORIAL_CAT = TUTORIAL_CAT)
 
-@app.route('/view')
+@flap.route('/view')
 def view():
 	# render the view post page
 	return render_template('view.html', title='Viewing Post')
@@ -75,11 +78,11 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/uploads/<filename>')
+@flap.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-@app.route('/dashboard/upload', methods=['GET', 'POST'])
+@flap.route('/dashboard/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -97,14 +100,14 @@ def upload_file():
             return render_template('dashboard/upload.html', title='Dashboard', filename = filename)
     return render_template('dashboard/upload.html', title='Dashboard')
 
-@app.route('/dashboard')
+@flap.route('/dashboard')
 def dashboard():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
         return render_template('dashboard.html', title='Dashboard')
 
-@app.route('/login', methods=['GET', 'POST'])
+@flap.route('/login', methods=['GET', 'POST'])
 def do_admin_login():
     if request.method == 'POST':
         if request.form['password'] == 'password' and request.form['username'] == 'admin':
@@ -117,12 +120,12 @@ def do_admin_login():
     else:
         return render_template('login.html')
 
-@app.route('/register', methods=['GET', 'POST'])
+@flap.route('/register', methods=['GET', 'POST'])
 def register():
 	# render the view register page
 	return render_template('create_account.html')
 
-@app.route("/logout")
+@flap.route("/logout")
 def logout():
     session['logged_in'] = False
     return redirect(url_for('home'))
@@ -132,69 +135,69 @@ def logout():
 
 
 
-@app.route("/tutorials/python/"+TOPIC_DICT["Python"][0][1], methods=['GET', 'POST'])
+@flap.route("/tutorials/python/"+TOPIC_DICT["Python"][0][1], methods=['GET', 'POST'])
 def Getting_Started_with_Python():
     return render_template("tutorials/Python/getting-started-with-python.html", curLink = TOPIC_DICT["Python"][0][1], curTitle=TOPIC_DICT["Python"][0][0],  nextLink = TOPIC_DICT["Python"][1][1], nextTitle = TOPIC_DICT["Python"][1][0], curTopic = "Python", title = TOPIC_DICT["Python"][0][0], TOPIC_DICT = TOPIC_DICT)
 
-@app.route("/tutorials/python/"+TOPIC_DICT["Python"][1][1], methods=['GET', 'POST'])
+@flap.route("/tutorials/python/"+TOPIC_DICT["Python"][1][1], methods=['GET', 'POST'])
 def Creating_a_Hello_World_program_in_Python():
     return render_template("tutorials/Python/hello-world.html", curLink = TOPIC_DICT["Python"][1][1], curTitle=TOPIC_DICT["Python"][1][0], curTopic = "Python", title = TOPIC_DICT["Python"][1][0], TOPIC_DICT = TOPIC_DICT, nextTitle = "None")
 	
 	
-@app.route("/tutorials/c++/"+TOPIC_DICT["C++"][0][1], methods=['GET', 'POST'])
+@flap.route("/tutorials/c++/"+TOPIC_DICT["C++"][0][1], methods=['GET', 'POST'])
 def Installing_the_Cpp_compiler_and_CodeBlocks_IDE():
     return render_template("tutorials/C++/installing-compiler-and-codeblocks-ide.html", curLink = TOPIC_DICT["C++"][0][1], curTitle=TOPIC_DICT["C++"][0][0],  nextLink = TOPIC_DICT["C++"][1][1], nextTitle = TOPIC_DICT["C++"][1][0], curTopic = "C++", title = TOPIC_DICT["C++"][0][0], TOPIC_DICT = TOPIC_DICT)
 
 
 
 
-@app.route("/tutorials/c++/"+TOPIC_DICT["C++"][1][1], methods=['GET', 'POST'])
+@flap.route("/tutorials/c++/"+TOPIC_DICT["C++"][1][1], methods=['GET', 'POST'])
 def Creating_a_Hello_World_program_in_Cpp():
     return render_template("tutorials/C++/hello-world.html", curLink = TOPIC_DICT["C++"][1][1], curTitle=TOPIC_DICT["C++"][1][0], curTopic = "C++", title = TOPIC_DICT["C++"][1][0], TOPIC_DICT = TOPIC_DICT, nextLink = TOPIC_DICT["C++"][2][1], nextTitle= TOPIC_DICT["C++"][2][0])
 
-@app.route("/tutorials/c++/"+TOPIC_DICT["C++"][2][1], methods=['GET', 'POST'])
+@flap.route("/tutorials/c++/"+TOPIC_DICT["C++"][2][1], methods=['GET', 'POST'])
 def Getting_user_input_and_variables():
     return render_template("tutorials/C++/user-input-and-variables.html", curLink = TOPIC_DICT["C++"][2][1], curTitle=TOPIC_DICT["C++"][2][0], curTopic = "C++", title = TOPIC_DICT["C++"][2][0], TOPIC_DICT = TOPIC_DICT, nextLink = TOPIC_DICT["C++"][3][1], nextTitle= TOPIC_DICT["C++"][3][0])
 
-@app.route("/tutorials/c++/"+TOPIC_DICT["C++"][3][1], methods=['GET', 'POST'])
+@flap.route("/tutorials/c++/"+TOPIC_DICT["C++"][3][1], methods=['GET', 'POST'])
 def Data_Types():
     return render_template("tutorials/C++/data-types.html", curLink = TOPIC_DICT["C++"][3][1], curTitle=TOPIC_DICT["C++"][3][0], curTopic = "C++", title = TOPIC_DICT["C++"][3][0], TOPIC_DICT = TOPIC_DICT, nextLink = TOPIC_DICT["C++"][4][1], nextTitle= TOPIC_DICT["C++"][4][0])
 
-@app.route("/tutorials/c++/"+TOPIC_DICT["C++"][4][1], methods=['GET', 'POST'])
+@flap.route("/tutorials/c++/"+TOPIC_DICT["C++"][4][1], methods=['GET', 'POST'])
 def Conditional_Statements():
     return render_template("tutorials/C++/conditional-statements.html", curLink = TOPIC_DICT["C++"][4][1], curTitle=TOPIC_DICT["C++"][4][0], curTopic = "C++", title = TOPIC_DICT["C++"][4][0], TOPIC_DICT = TOPIC_DICT, nextLink = TOPIC_DICT["C++"][5][1], nextTitle = TOPIC_DICT["C++"][5][0])
 
-@app.route("/tutorials/c++/"+TOPIC_DICT["C++"][5][1], methods=['GET', 'POST'])
+@flap.route("/tutorials/c++/"+TOPIC_DICT["C++"][5][1], methods=['GET', 'POST'])
 def Loops():
     return render_template("tutorials/C++/loops.html", curLink = TOPIC_DICT["C++"][5][1], curTitle=TOPIC_DICT["C++"][5][0], curTopic = "C++", title = TOPIC_DICT["C++"][5][0], TOPIC_DICT = TOPIC_DICT, nextLink=TOPIC_DICT["C++"][6][1], nextTitle= TOPIC_DICT["C++"][6][0])
 	
 
-@app.route("/tutorials/c++/"+TOPIC_DICT["C++"][6][1], methods=['GET', 'POST'])
+@flap.route("/tutorials/c++/"+TOPIC_DICT["C++"][6][1], methods=['GET', 'POST'])
 def How_to_create_and_use_functions():
     return render_template("tutorials/C++/how-to-create-and-use-functions.html", curLink = TOPIC_DICT["C++"][6][1], curTitle=TOPIC_DICT["C++"][6][0], curTopic = "C++", title = TOPIC_DICT["C++"][6][0], TOPIC_DICT = TOPIC_DICT, nextTitle= "None")
 	
 	
 	
-@app.route("/tutorials/microsoft windows/"+TOPIC_DICT["Microsoft Windows"][0][1], methods=['GET', 'POST'])
+@flap.route("/tutorials/microsoft windows/"+TOPIC_DICT["Microsoft Windows"][0][1], methods=['GET', 'POST'])
 def How_to_use_Linux_terminal_commands_in_Windows():
     return render_template("tutorials/Microsoft Windows/how-to-use-linux-terminal-commands-in-windows.html", curLink = TOPIC_DICT["Microsoft Windows"][0][1], curTitle=TOPIC_DICT["Microsoft Windows"][0][0], nextTitle = "None", curTopic = "Microsoft Windows", title = TOPIC_DICT["Microsoft Windows"][0][0], TOPIC_DICT = TOPIC_DICT)
 
 
-@app.route("/tutorials/miscellaneous/"+TOPIC_DICT["Miscellaneous"][0][1], methods=['GET', 'POST'])
+@flap.route("/tutorials/miscellaneous/"+TOPIC_DICT["Miscellaneous"][0][1], methods=['GET', 'POST'])
 def How_to_install_Exodus_on_Kodi():
     return render_template("tutorials/Miscellaneous/how-to-install-exodus-on-kodi.html", curLink = TOPIC_DICT["Miscellaneous"][0][1], curTitle=TOPIC_DICT["Miscellaneous"][0][0], nextTitle = "None", curTopic = "Miscellaneous", title = TOPIC_DICT["Miscellaneous"][0][0], TOPIC_DICT = TOPIC_DICT)
 	
 	
-@app.route("/blog/"+BLOG_TOPIC_DICT["blog"][0][1], methods=['GET', 'POST'])
+@flap.route("/blog/"+BLOG_TOPIC_DICT["blog"][0][1], methods=['GET', 'POST'])
 def Programming_in_Python():
     return render_template("blog/programming-in-python.html", curLink = BLOG_TOPIC_DICT["blog"][0][1], curTitle=BLOG_TOPIC_DICT["blog"][0][0], curTopic = "blog", title = BLOG_TOPIC_DICT["blog"][0][0], BLOG_TOPIC_DICT = BLOG_TOPIC_DICT, TUTORIAL_CAT = TUTORIAL_CAT, nextTitle= "None")
 
-@app.route('/robots.txt')
-@app.route('/sitemap.xml')
-@app.route('/BingSiteAuth.xml')
+@flap.route('/robots.txt')
+@flap.route('/sitemap.xml')
+@flap.route('/BingSiteAuth.xml')
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    flap.run(host='0.0.0.0', port=port)
