b9793fb80da9a1125bf9ebc562f33023dcd43125
nmarriotti <nmarriotti@gmail.com>
2017-05-28 06:28:04 -0700

    updated

--- app.py
+++ app.py
@@ -190,8 +190,12 @@ def Writing_to_a_File():
 
 @app.route("/tutorials/python/"+TOPIC_DICT["Python"][11][1], methods=['GET', 'POST'])
 def Reading_from_a_File():
-    return render_template("tutorials/Python/reading-from-a-file.html", curLink = TOPIC_DICT["Python"][11][1], curTitle=TOPIC_DICT["Python"][11][0], curTopic = "Python", title = TOPIC_DICT["Python"][11][0], TOPIC_DICT = TOPIC_DICT, nextTitle="None")
+    return render_template("tutorials/Python/reading-from-a-file.html", curLink = TOPIC_DICT["Python"][11][1], curTitle=TOPIC_DICT["Python"][11][0], curTopic = "Python", title = TOPIC_DICT["Python"][11][0], TOPIC_DICT = TOPIC_DICT, nextTitle=TOPIC_DICT["Python"][12][0], nextLink=TOPIC_DICT["Python"][12][1])
 
+
+@app.route("/tutorials/python/"+TOPIC_DICT["Python"][12][1], methods=['GET', 'POST'])
+def DOCSTRING():
+    return render_template("tutorials/Python/docstring.html", curLink = TOPIC_DICT["Python"][12][1], curTitle=TOPIC_DICT["Python"][12][0], curTopic = "Python", title = TOPIC_DICT["Python"][12][0], TOPIC_DICT = TOPIC_DICT, nextTitle= "None")
 
 
 @app.route("/tutorials/c/"+TOPIC_DICT["C"][0][1], methods=['GET', 'POST'])
