a0e7791c03a9342ee5f404c09a3786552df0818e
nmarriotti <nmarriotti@gmail.com>
2017-06-08 23:33:00 -0700

    updated

--- app.py
+++ app.py
@@ -365,7 +365,12 @@ def How_to_install_Exodus_on_Kodi():
 	
 @app.route("/tutorials/miscellaneous/"+TOPIC_DICT["Miscellaneous"][1][1], methods=['GET', 'POST'])
 def How_to_setup_DoD_CACPKI_on_Chrome_Ubunutu_Linux():
-    return render_template("tutorials/Miscellaneous/how-to-setup-dod-cac-pki-on-chrome-ubuntu-linux.html", curLink = TOPIC_DICT["Miscellaneous"][1][1], curTitle=TOPIC_DICT["Miscellaneous"][1][0], nextTitle = "None", curTopic = "Miscellaneous", title = TOPIC_DICT["Miscellaneous"][1][0], TOPIC_DICT = TOPIC_DICT)
+    return render_template("tutorials/Miscellaneous/how-to-setup-dod-cac-pki-on-chrome-ubuntu-linux.html", curLink = TOPIC_DICT["Miscellaneous"][1][1], curTitle=TOPIC_DICT["Miscellaneous"][1][0], nextTitle = TOPIC_DICT["Miscellaneous"][2][0], curTopic = "Miscellaneous", title = TOPIC_DICT["Miscellaneous"][1][0], TOPIC_DICT = TOPIC_DICT, nextLink=TOPIC_DICT["Miscellaneous"][2][1])
+
+
+@app.route("/tutorials/miscellaneous/"+TOPIC_DICT["Miscellaneous"][2][1], methods=['GET', 'POST'])
+def Best_website_to_stream_live_sports():
+    return render_template("tutorials/Miscellaneous/best-website-to-stream-live-sports.html", curLink = TOPIC_DICT["Miscellaneous"][2][1], curTitle=TOPIC_DICT["Miscellaneous"][2][0], curTopic = "Miscellaneous", title = TOPIC_DICT["Miscellaneous"][2][0], TOPIC_DICT = TOPIC_DICT, nextTitle= "None")
 
 
 @app.route("/tutorials/java/"+TOPIC_DICT["Java"][0][1], methods=['GET', 'POST'])
